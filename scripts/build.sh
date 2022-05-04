acc329ba0c0d7deab4612f203fe34f1cf6d55510
Alexey Gonchar <tumberum@gmail.com>
2022-05-04 19:44:43 +0700

    fnfn

--- scripts/build.sh
+++ scripts/build.sh
@@ -78,8 +78,6 @@ done
 # Building image
 source $envfile
 
-docker run -d -p 5000:5000 --restart=always --name registry registry:2
-
 docker buildx build . \
   --file "$dockerfile" \
   --tag="${APP_NAME}" \
